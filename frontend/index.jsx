ba22b0ca682409ffb5b97d1bdd4e4c01bddd1141
Alex Crooks <alexcrooks2@gmail.com>
2021-09-27 10:50:21 -0700

    User auth almost complete

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import createStore from './store/store';
+import configureStore from './store/store';
 import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root")
-    ReactDOM.render(<Root />, root)
+    const store = configureStore()
+    ReactDOM.render(<Root store = {store}/>, root)
 })
