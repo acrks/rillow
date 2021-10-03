537cfa73dda18e011f4a1b1f4cf847cff2337fc8
Alex Crooks <alexcrooks2@gmail.com>
2021-10-03 12:21:19 -0700

    To get github link

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Listing.destroy_all
-User.destroy_all
+Listing.delete_all
+User.delete_all
 Favorite.destroy_all
 
 user1 = User.create!(email: "chumbawumba@gmail.com", password: "password")
