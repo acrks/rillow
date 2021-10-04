7ab585b7b6e2d37a5df52ed56095a096d38a1fb1
Alex Crooks <alexcrooks2@gmail.com>
2021-10-04 14:05:25 -0700

    Fixed hilarious links

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 export default ({ currentUser, logout, openModal }) => {
   return (
     <div className = "nav-bar">
-    <Link to="/listings" className = "nav-bar-link">Buy</Link>
-    <Link to = "/listings" className = "nav-bar-link">Rent</Link>
-    <Link to = "/listings/create" className = "nav-bar-link">Sell</Link>
-    <Link to = "/https://www.linkedin.com/in/alex-crooks/" className = "nav-bar-link">Get a cash offer</Link>
-    <Link to = "/https://account.venmo.com/u/acrks" className = "nav-bar-link">Home Loans</Link>
-    <Link to = "/" className="logo">Rillow</Link>
-    <Link to = "/https://www.fbi.gov/" className = "nav-bar-link">Agent Finder</Link>
-    <Link to = "/" className = "nav-bar-link">Manage Rentals</Link>
-    <Link to = "/https://alexcrooks.net/" className = "nav-bar-link">Advertise</Link>
-    <Link to = "/https://stackoverflow.com/" className = "nav-bar-link">Help</Link>
-    {currentUser ? <button onClick = {logout} className = "session-button">Log Out</button> : 
-    <button onClick={() => openModal('login')} className = "session-button">Login</button>} 
-  </div>
+      <Link to="/listings" className = "nav-bar-link">Buy</Link>
+      <Link to = "/listings" className = "nav-bar-link">Rent</Link>
+      <Link to = "/listings/create" className = "nav-bar-link">Sell</Link>
+      <a target="_blank" href="https://www.linkedin.com/in/alex-crooks/" className = "nav-bar-link">Give a cash offer</a>
+      <a target="_blank" href="https://account.venmo.com/u/acrks" className = "nav-bar-link">Home Loans</a>
+      <Link to = "/" className="logo">Rillow</Link>
+      <a target="_blank" href="https://www.fbi.gov/" className = "nav-bar-link">Agent finder</a>
+      <Link to = "/" className = "nav-bar-link">Manage Rentals</Link>
+      <a target="_blank" href="https://alexcrooks.net/" className = "nav-bar-link">Advertise</a>
+      <a target="_blank" href="https://stackoverflow.com/" className = "nav-bar-link">Help</a>
+      {currentUser ? <button onClick = {logout} className = "session-button">Log Out</button> : 
+      <button onClick={() => openModal('login')} className = "session-button">Login</button>} 
+    </div>
   );
 };
