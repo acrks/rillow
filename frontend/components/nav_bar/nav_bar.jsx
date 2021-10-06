d33fbfb06550a469525941fdf2605bff9e4d0edc
Alex Crooks <alexcrooks2@gmail.com>
2021-10-05 22:57:52 -0700

    Cleaned up links

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -7,13 +7,12 @@ export default ({ currentUser, logout, openModal }) => {
       <Link to="/listings" className = "nav-bar-link">Buy</Link>
       <Link to = "/listings" className = "nav-bar-link">Rent</Link>
       <Link to = "/listings/create" className = "nav-bar-link">Sell</Link>
-      <a target="_blank" href="https://www.linkedin.com/in/alex-crooks/" className = "nav-bar-link">Give a cash offer</a>
+      <a target="_blank" href="https://www.linkedin.com/in/alex-crooks/" className = "nav-bar-link">LinkedIn</a>
       <a target="_blank" href="https://account.venmo.com/u/acrks" className = "nav-bar-link">Home Loans</a>
       <Link to = "/" className="logo">Rillow</Link>
-      <a target="_blank" href="https://www.fbi.gov/" className = "nav-bar-link">Agent finder</a>
       <Link to = "/" className = "nav-bar-link">Manage Rentals</Link>
-      <a target="_blank" href="https://alexcrooks.net/" className = "nav-bar-link">Advertise</a>
-      <Link to = "/user/favorites" className = "nav-bar-link">Saved homes</Link>
+      <a target="_blank" href="https://alexcrooks.net/" className = "nav-bar-link">Portfolio</a>
+      {/* <Link to = "/user/favorites" className = "nav-bar-link">Saved homes</Link> */}
       <a target="_blank" href="https://stackoverflow.com/" className = "nav-bar-link">Help</a>
       {currentUser ? <button onClick = {logout} className = "session-button">Log Out</button> : 
       <button onClick={() => openModal('login')} className = "session-button">Login</button>} 
