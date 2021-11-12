80d7063311c505734387225044e3147b82a28ad0
Alex Crooks <alexcrooks2@gmail.com>
2021-11-11 23:22:40 -0800

    No really, this is the last commit, I'm going to bed

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,8 +7,9 @@ export default () => {
             <a target="_blank" href="https://alexcrooks.net/" className = "nav-bar-link">My Portfolio</a>
             <a target="_blank" href="https://github.com/acrks/aa_js_project" className = "nav-bar-link">JavaScript project</a>
             <a target="_blank" href="https://pacific-sea-61217.herokuapp.com/#/" className = "nav-bar-link">MERN project</a>
-            <a target="_blank" href="https://github.com/acrks/" className = "logo-text">My Other Projects</a>
-            <a target="_blank" href="https://github.com/acrks/BookStore" className = "nav-bar-link">Java Facebook clone</a>
+            <a target="_blank" href="https://github.com/acrks/" className = "logo-text">About the Developer</a>
+            <a target="_blank" href="https://github.com/acrks/BookStore" className = "nav-bar-link">Java Bookstore</a>
+            <a target="_blank" href="https://github.com/acrks/JavaFacebookClone" className = "nav-bar-link">Java Facebook clone</a>
             <a target="_blank" href="https://github.com/acrks/pythonGoogleClone" className = "nav-bar-link">Python Search Engine</a>
             <a target="_blank" href="https://github.com/acrks/4ambounce.com" className = "nav-bar-link">Custom MP3 player</a>  
         </div>
