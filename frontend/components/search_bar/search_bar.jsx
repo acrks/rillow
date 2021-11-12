b2e31577bb329caae8d6f59960c28690fd0f47e0
Alex Crooks <alexcrooks2@gmail.com>
2021-11-11 17:07:09 -0800

    Hitting enter triggers search

--- frontend/components/search_bar/search_bar.jsx
+++ frontend/components/search_bar/search_bar.jsx
@@ -9,19 +9,33 @@ class SearchBar extends React.Component {
         }
 
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleKeypress = this.handleKeypress.bind(this)
     }
 
     handleChange(e) {
         return e => {this.setState({searchTerm: e.currentTarget.value})}
     }
+
+    handleSubmit(e) {
+        this.props.history.push({pathname: "/listings/", searchTerm: this.state.searchTerm})
+    }
+
+    handleKeypress(e){
+      //it triggers by pressing the enter key
+      console.log(e)
+        if (e.key === "Enter") {
+        this.handleSubmit();
+        }
+    };
     
     render() {
         return (
             <div className = "search-bar">
                 <span className = "headline home-headline">Change starts here</span>
             <form className = "search-bar-element">
-                <input type = "text" placeholder = "Enter a street name, city, or ZIP code" value = {this.state.searchTerm} onChange = {this.handleChange()}/>
-                <button type="submit"><i className="fa fa-search" onClick = {() => this.props.history.push({pathname: "/listings/", searchTerm: this.state.searchTerm})}></i></button>
+                <input type = "text" placeholder = "Enter a street name, city, or ZIP code" value = {this.state.searchTerm} onChange = {this.handleChange()} onKeyPress = {this.handleKeypress}/>
+                <button type="submit"><i className="fa fa-search" onClick = {this.handleSubmit}></i></button>
             </form>
             </div>
         )
