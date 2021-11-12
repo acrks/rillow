5e0507e7154e24252f2c9d9bafc9ab0ad0ec60ca
Alex Crooks <alexcrooks2@gmail.com>
2021-11-11 23:16:24 -0800

    New footer element

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,7 +9,7 @@ export default () => {
             <a target="_blank" href="https://pacific-sea-61217.herokuapp.com/#/" className = "nav-bar-link">MERN project</a>
             <a target="_blank" href="https://github.com/acrks/" className = "logo-text">My Other Projects</a>
             <a target="_blank" href="https://github.com/acrks/BookStore" className = "nav-bar-link">Java Facebook clone</a>
-            <a target="_blank" href="https://github.com/acrks/BookStore" className = "nav-bar-link">Python Search Engine</a>
+            <a target="_blank" href="https://github.com/acrks/pythonGoogleClone" className = "nav-bar-link">Python Search Engine</a>
             <a target="_blank" href="https://github.com/acrks/4ambounce.com" className = "nav-bar-link">Custom MP3 player</a>  
         </div>
     )
